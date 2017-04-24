ba9d9b94fd1403e83e6c7a78cc99677fea2f8aa0
root <root@hpss.ncsa.illinois.edu>
2017-04-24 15:28:44 -0500

    dkr initial refs git@github.com:JasonAlt/GridFTP-DSI-for-HPSS.git

--- source/loaders/version.h
+++ source/loaders/version.h
@@ -38,60 +38,18 @@
  * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
  * DEALINGS WITH THE SOFTWARE.
  */
-#ifndef HPSS_DSI_RETR_H
-#define HPSS_DSI_RETR_H
 
 /*
- * System includes
+ * Globus includes.
  */
-#include <pthread.h>
+#include <globus_module.h>
 
-/*
- * Globus includes
- */
-#include <globus_gridftp_server.h>
-#include <globus_list.h>
+static
+globus_version_t version = 
+{
+    2,
+    0,
+    1493065013,
+    99999
+};
 
-/*
- * Local includes
- */
-#include "pio.h"
-
-struct retr_info;
-
-typedef struct {
-    char             * Buffer;
-    struct retr_info * RetrInfo;
-#define VALID_TAG   0xDEADBEEF
-#define INVALID_TAG 0x00000000
-    int                Valid; // Debug Entry
-} retr_buffer_t;
-
-typedef struct retr_info {
-	globus_gfs_operation_t       Operation;
-	globus_gfs_transfer_info_t * TransferInfo;
-
-	int      FileFD;
-	uint64_t FileSize;
-
-	globus_result_t Result;
-	globus_size_t   BlockSize;
-	globus_off_t    RangeLength;
-	globus_off_t    CurrentOffset;
-
-	pthread_mutex_t Mutex;
-	pthread_cond_t  Cond;
-
-	int OptConnCnt;
-	int ConnChkCnt;
-
-	globus_list_t * AllBufferList;
-	globus_list_t * FreeBufferList;
-
-} retr_info_t;
-
-void
-retr(globus_gfs_operation_t       Operation,
-     globus_gfs_transfer_info_t * TransferInfo);
-
-#endif /* HPSS_DSI_RETR_H */
